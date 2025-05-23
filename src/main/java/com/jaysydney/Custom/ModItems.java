2bf10b8345c0bb6b26b60e301aac56c2359a3707
AnOverlyComplexUsername <AnOverlyComplexEmail@gmail.com>
2025-05-23 13:12:33 -0500

    eatting fumo gives you blindness

--- src/main/java/com/jaysydney/Custom/ModItems.java
+++ src/main/java/com/jaysydney/Custom/ModItems.java
@@ -5,9 +5,14 @@ import java.util.function.Function;
 import com.jaysydney.HerobrineComedyMod;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.component.type.ConsumableComponent;
+import net.minecraft.component.type.ConsumableComponents;
 import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -15,8 +20,14 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
+    public static final ConsumableComponent BLIND_FOOD_CONSUMABLE_COMPONENT = ConsumableComponents.food()
+            // The duration is in ticks, 20 ticks = 1 second
+            .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.DARKNESS, 3000 * 20, 3), 1.0f))
+            .build();
+
     public static final Item SUSPICIOUS_SUBSTANCE = register("suspicious_substance", Item::new,
-            new Item.Settings().food(new FoodComponent.Builder().nutrition(3).saturationModifier(5).build()));
+            new Item.Settings().food(new FoodComponent.Builder().nutrition(3).saturationModifier(5).build(), BLIND_FOOD_CONSUMABLE_COMPONENT));
+
 
     public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
         // Create the item key.
@@ -32,7 +43,7 @@ public class ModItems {
     }
 
     public static void initialize() {
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK)
                 .register((itemGroup) -> itemGroup.add(ModItems.SUSPICIOUS_SUBSTANCE));
     }
 
